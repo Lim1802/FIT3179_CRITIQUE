03fac174b426d3dabf02696e753f01c82cfcbaa9
Lim Jun Kee <101441406+Lim1802@users.noreply.github.com>
2024-10-09 01:34:05 +0800

    Add files via upload

---
+++ map.js
@@ -0,0 +1,10 @@
+
+
+var specVis1 = "map.json";
+var specVis2 = "scatterPlot.json";
+var specVis3 = "smallMultiples.json";
+var specVis4 = "radialChart.json";
+vegaEmbed('#vis',specVis1);
+vegaEmbed('#vis2',specVis2);
+vegaEmbed('#vis3',specVis3);
+vegaEmbed('#vis4',specVis4);
